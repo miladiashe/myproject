d13795867ebe209a4e9dc7ac37b4095cb420f696
miladiashe <dkdud0122@naver.com>
2021-12-31 04:02:57 +0900

    답변 모델
    공부중

--- pybo/models.py
+++ pybo/models.py
@@ -5,3 +5,10 @@ class Question(db.model):
     subject = db.Column(db.string(200), nullable=False)
     contemt = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.Datetime(), nullable=False)
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Foreignkey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set'))
+    contemt = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.Datetime(), nullable=False)
