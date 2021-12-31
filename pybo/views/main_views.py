c43ddf955244a26f1c26348656b50c21181c5fe0
miladiashe <dkdud0122@naver.com>
2021-12-31 12:04:24 +0900

    파일 분리
    플라스크 재밌다

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -8,15 +8,6 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def hello_pybo():
     return 'Hello, Pybo!'
 
-@bp.route('/')
-def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    #질문 목록 데이터를 쿼리로 받아오는 것
-    return render_template('question/question_list.html', question_list=question_list)
 
-@bp.route('/detail/<int:question_id>/')
-def detail(question_id):
-    question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
 
 
