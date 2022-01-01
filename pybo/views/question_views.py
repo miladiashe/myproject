35fd051c8638b5a09fc1250ddcbf623966084e3b
miladiashe <dkdud0122@naver.com>
2022-01-01 17:28:37 +0900

    이유를 알 수 없는 버그 발생

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, url_for
 from werkzeug.utils import redirect
 
 from .. import db
-from ..forms import QuestionForm
+from ..forms import QuestionForm, AnswerForm
 from pybo.models import Question
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -18,8 +18,9 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form=AnswerForm
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
 
 @bp.route('/create/', methods=('GET', 'POST'))
