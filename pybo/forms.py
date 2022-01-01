8651729ecd8c3f50e3bc172b4315e8cf859a2a3e
miladiashe <dkdud0122@naver.com>
2022-01-01 15:50:42 +0900

    질문 기능 수정

--- pybo/forms.py
+++ pybo/forms.py
@@ -2,7 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
+
 class QuestionForm(FlaskForm):
-    subject = StringField('제목', validators=[DataRequired()])
-    content = TextAreaField('내용', validators=[DataRequired()])
+    subject = StringField('제목', validators=[DataRequired('제목을 입력해 주세요.')])
+    content = TextAreaField('내용', validators=[DataRequired('내용을 입력해 주세요.')])
 
